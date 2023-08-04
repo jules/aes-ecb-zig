190422cc14039b0eaa238d878d3c414399127e5c
Jules de Smit <julesdesmit@gmail.com>
2023-08-04 18:19:56 +0200

    Add state manipulation functions

--- src/main.zig
+++ src/main.zig
@@ -2,14 +2,15 @@ const std = @import("std");
 const key_schedule = @import("key_schedule.zig");
 const padding = @import("padding.zig");
 const State = @import("state.zig").State;
+const state_size = @import("state.zig").state_size;
 
-const AesError = error {
+const AesError = error{
     InvalidKeyLength,
-}
+};
 
-export fn encrypt(plaintext: []const u8, key: []const u8) ![]u8 {
-    if (key.len > 16) {
-        return AesError::InvalidKeyLength;
+export fn encrypt(plaintext: []const u8, key: []const u8, allocator: std.mem.Allocator) ![]u8 {
+    if (key.len > state_size) {
+        return AesError.InvalidKeyLength;
     }
 
     const padded_key = padding.padKey(key);
@@ -19,28 +20,30 @@ export fn encrypt(plaintext: []const u8, key: []const u8) ![]u8 {
     const keys = key_schedule.keyExpansion(padded_key);
 
     var ciphertext = try allocator.alloc(u8, padded_plaintext.len);
-    const repetitions = padded_plaintext / 16;
+    const repetitions = padded_plaintext / state_size;
     for (0..repetitions) |i| {
-        var state = State.init(padded_plaintext[i*16..i*16+16]);
-        state.addRoundKey(keys[0..16]);
+        var state = State.init(padded_plaintext[i * state_size .. i * state_size + state_size]);
+        state.addRoundKey(keys[0..state_size]);
 
         for (1..10) |j| {
             state.subBytes();
             state.shiftRows();
             state.mixColumns();
-            state.addRoundKey(keys[j*16..j*16+16);
+            state.addRoundKey(keys[j * state_size .. j * state_size + state_size]);
         }
 
         state.subBytes();
         state.shiftRows();
-        state.addRoundKey(keys[keys.len-16..keys.len]);
+        state.addRoundKey(keys[keys.len - state_size .. keys.len]);
 
-        ciphertext[i*16..i*16+16] = state;
+        for (i * state_size..i * state_size + state_size, 0..) |j, k| {
+            ciphertext[j] = state.mat[k];
+        }
     }
 
     return ciphertext;
 }
 
-export fn decrypt(ciphertext: []const u8, key: []const u8) ![]u8 {
-
-}
+//export fn decrypt(ciphertext: []const u8, key: []const u8) ![]u8 {
+//
+//}
--- src/state.zig
+++ src/state.zig
@@ -1,11 +1,14 @@
 const std = @import("std");
+const transformations = @import("transformations.zig");
+
+const state_size: usize = 16;
 
 const State = struct {
-    mat: [16]u8,
+    mat: [state_size]u8,
 
-    pub fn init(in: [16]u8) State {
+    pub fn init(in: [state_size]u8) State {
         var self = State{
-            .mat = [16]u8{
+            .mat = [state_size]u8{
                 0, 0, 0, 0,
                 0, 0, 0, 0,
                 0, 0, 0, 0,
@@ -17,9 +20,49 @@ const State = struct {
         return self;
     }
 
-    pub fn addRoundKey(self: State, key: [16]u8) void {
+    pub fn addRoundKey(self: State, key: [state_size]u8) void {
         for (self.mat, key, 0..) |state, k, i| {
             self.mat[i] = state ^ k;
         }
     }
+
+    pub fn subBytes(self: State) void {
+        for (self.mat, 0..) |byte, i| {
+            self.mat[i] = transformations.subByte(byte);
+        }
+    }
+
+    pub fn shiftRows(self: State) void {
+        var offset: usize = 4;
+        for (1..4) |i| {
+            var shift: [4]u8 = undefined;
+            // TODO: lol, lmao
+            for (0..4) |j| {
+                shift[j] = self.mat[i + offset * i + j * 4 % state_size];
+            }
+
+            for (0..4) |j| {
+                self.mat[i + j * offset] = shift[j];
+            }
+        }
+    }
+
+    pub fn mixColumns(self: State) void {
+        const muls_1 = @Vector(4, u8){ 2, 3, 1, 1 };
+        const muls_2 = @Vector(4, u8){ 1, 2, 3, 1 };
+        const muls_3 = @Vector(4, u8){ 1, 1, 2, 3 };
+        const muls_4 = @Vector(4, u8){ 3, 1, 1, 2 };
+        for (0..4) |i| {
+            const state_col: @Vector(4, u8) = self.mat[i * 4 .. i * 4 + 4];
+            self.mat[i * 4] = row_mul_into_xor(state_col, muls_1);
+            self.mat[i * 4] = row_mul_into_xor(state_col, muls_2);
+            self.mat[i * 4] = row_mul_into_xor(state_col, muls_3);
+            self.mat[i * 4] = row_mul_into_xor(state_col, muls_4);
+        }
+    }
 };
+
+fn row_mul_into_xor(state: @Vector(4, u8), muls: @Vector(4, u8)) u8 {
+    const result_col = state * muls;
+    return result_col[0] ^ result_col[1] ^ result_col[2] ^ result_col[3];
+}
