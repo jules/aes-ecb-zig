93577aa4c4b778cd971e46fb164bbc2c0b5d509f
Jules de Smit <julesdesmit@gmail.com>
2023-09-22 14:15:02 +0200

    fix mixcolumns

--- src/state.zig
+++ src/state.zig
@@ -55,13 +55,13 @@ pub const State = struct {
     }
 
     pub fn mixColumns(self: *State) void {
-        const muls_1 = @Vector(4, u8){ 2, 3, 1, 1 };
-        const muls_2 = @Vector(4, u8){ 1, 2, 3, 1 };
-        const muls_3 = @Vector(4, u8){ 1, 1, 2, 3 };
-        const muls_4 = @Vector(4, u8){ 3, 1, 1, 2 };
+        const muls_1 = [4]u8{ 2, 3, 1, 1 };
+        const muls_2 = [4]u8{ 1, 2, 3, 1 };
+        const muls_3 = [4]u8{ 1, 1, 2, 3 };
+        const muls_4 = [4]u8{ 3, 1, 1, 2 };
         const slice: []const u8 = &self.mat;
         for (0..4) |i| {
-            const state_col: @Vector(4, u8) = slice[i * 4 ..][0..4].*;
+            const state_col: [4]u8 = slice[i * 4 ..][0..4].*;
             self.mat[i * 4] = row_mul_into_xor(state_col, muls_1);
             self.mat[i * 4 + 1] = row_mul_into_xor(state_col, muls_2);
             self.mat[i * 4 + 2] = row_mul_into_xor(state_col, muls_3);
@@ -107,7 +107,33 @@ pub const State = struct {
     }
 };
 
-fn row_mul_into_xor(state: @Vector(4, u8), muls: @Vector(4, u8)) u8 {
-    const result_col = @mulWithOverflow(state, muls);
-    return result_col[0][0] ^ result_col[0][1] ^ result_col[0][2] ^ result_col[0][3];
+fn row_mul_into_xor(state: [4]u8, muls: [4]u8) u8 {
+    return multiply(state[0], muls[0]) ^ multiply(state[1], muls[1]) ^ multiply(state[2], muls[2]) ^ multiply(state[3], muls[3]);
+}
+
+fn multiply(num: u8, mul: u8) u8 {
+    // since we have constant multiplication terms and multiplication isn't straightforward,
+    // we instead just switch on different constants and apply the right arithmetic.
+    return switch (mul) {
+        0x01 => num,
+        0x02 => mul_by_2(num),
+        0x03 => mul_by_2(num) ^ num,
+        0x09 => mul_by_2(mul_by_2(mul_by_2(num))) ^ num,
+        0x0b => mul_by_2(mul_by_2(mul_by_2(num))) ^ mul_by_2(num) ^ num,
+        0x0d => mul_by_2(mul_by_2(mul_by_2(num))) ^ mul_by_2(mul_by_2(num)) ^ num,
+        0x0e => mul_by_2(mul_by_2(mul_by_2(num))) ^ mul_by_2(mul_by_2(num)) ^ mul_by_2(num),
+        else => @panic("unexpected constant"),
+    };
+}
+
+// GF(2^8) multiplication by 2
+fn mul_by_2(num: u8) u8 {
+    var shifted_num = num << 1;
+    // if we overflow, we need to reduce mod x^8 + x^4 + x^3 + x + 1
+    if (num & 0b10000000 == 0b10000000) {
+        // since x^8 already gets chopped off from the bitshift, we can just XOR by 0b00011011
+        shifted_num ^= 0b00011011;
+    }
+
+    return shifted_num;
 }
